3f6cd7de557e69e050bcd9a3a84df10a383802ba
Franco Borgiani <franco.borgiani@delto.com>
2026-05-24 14:22:01 -0300

    add terminal theme and extra vim confs

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,7 +1,9 @@
 vim.cmd("set expandtab")
+vim.opt.incsearch = true
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.opt.relativenumber = true
+vim.opt.incsearch = true
 
 
