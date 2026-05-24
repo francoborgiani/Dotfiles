d4c7de22cf36da1b601e3c23e479a36aa56bcb16
Franco Borgiani <franco.borgiani@delto.com>
2026-05-24 15:18:30 -0300

    add nice nvim remaps

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,4 +6,12 @@ vim.cmd("set shiftwidth=4")
 vim.opt.relativenumber = true
 vim.opt.incsearch = true
 
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
 
